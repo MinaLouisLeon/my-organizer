66646a13ec98d1d576feaef09518ef67ca03d061
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2021-10-12 09:19:20 -0700

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   //add firebase configuration
-  apiKey: "AIzaSyBBopnU8FcxMXFzLV17ezzLjZ-I--4RAUo",
-  authDomain: "my-organizer-fcdfa.firebaseapp.com",
-  projectId: "my-organizer-fcdfa",
-  storageBucket: "my-organizer-fcdfa.appspot.com",
-  messagingSenderId: "158782845138",
-  appId: "1:158782845138:web:03fc342cce91057b78bd0b",
-  measurementId: "G-MHKQN0GRJC",
+
 };
 
 export const app = initializeApp(firebaseConfig);
